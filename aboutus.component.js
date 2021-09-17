f48cc7d9feaf6727f2aaa4687f83828d82ce8ac2
vinaykumarerla <90862560+vinaykumarerla@users.noreply.github.com>
2021-09-17 16:12:49 -0400

    Add files via upload

---
+++ aboutus.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class AboutUs extends Component {
+    render() {
+        return (
+            <div>
+                <h3>COMING SOON</h3>
+            </div>
+        );
+    }
+}
